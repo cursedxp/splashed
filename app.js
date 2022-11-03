38582725891ec789f6ecdd31fd6bc237ee7f766d
cursedxp <anilozsoy@gmail.com>
2022-11-03 14:56:52 +0100

    testing handle bar and views

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 //Express server
 const express = require("express");
+//Path api for node
+const path = require("path");
+//Hbs template engine for express
+const hbs = require("hbs");
 const app = express();
 const port = 3000;
 
 //Serving static files in Express
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Serving handlebars
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 //Testing express server
 app.get("/", (req, res) => {
-  res.send(`Server is online`);
+  res.render("home");
 });
 
 app.listen(port, () => {
