2742c56a23e51b2ab25860bc08bf51a9216407eb
Anil Ozsoy <anilozsoy@gmail.com>
2022-12-16 13:22:15 +0100

    add image method has been added to canvas

--- Canvas.js
+++ Canvas.js
@@ -1,16 +1,9 @@
 export default class Canvas {
-  constructor(width, height) {
-    this.properties = {
-      width: width,
-      height: height,
-    };
-  }
-
   //Creates canvas
-  createCanvas() {
+  constructor() {
     const canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
-
+    this.canvasContext = canvas.getContext("2d");
     this.resizeCanvas();
   }
   //Resizes the canvas based on browser size
@@ -23,8 +16,14 @@ export default class Canvas {
 
   //style canvas bg to grey or custom color
   styleCanvas(canvas, width, height) {
-    const canvasContext = canvas.getContext("2d");
-    canvasContext.fillStyle = "rgb(242, 242, 242)";
-    canvasContext.fillRect(0, 0, width, height);
+    this.canvasContext.fillStyle = "rgb(242, 242, 242)";
+    this.canvasContext.fillRect(0, 0, width, height);
+  }
+  addImage(url, file) {
+    let image = Image();
+    image.src = url;
+    image.onload = () => {
+      this.canvasContext.drawImage(image, 0, 0);
+    };
   }
 }
